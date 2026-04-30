9a94d9c854c073f964dcf0447b769c8e7e6ca2a1
Sumukha <sumukhadhandapani@gmail.com>
2026-04-30 11:41:26 +0530

    fix: add tailwind to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
+    tailwindcss(),
     react(),
   ],
   resolve: {
